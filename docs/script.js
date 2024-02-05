d64328254affebb0c557a01cf8bb3e2b9af8b0a5
Tatianafortech <111741943+Tatianafortech@users.noreply.github.com>
2024-02-05 18:35:52 +0200

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // This function will be executed when the DOM is fully loaded
+
+    // Example: Display an alert
+    alert("Hello from script.js! This is a simple GitHub Pages app.");
+});
